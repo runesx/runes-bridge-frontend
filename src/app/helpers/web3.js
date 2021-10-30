737f3c7ad30cc810651aaa654e8aa96291ead6af
bobpepers <bob.pepers@gmail.com>
2021-10-30 23:54:52 +0200

    up

--- src/app/helpers/web3.js
+++ src/app/helpers/web3.js
@@ -2,6 +2,6 @@ import Web3 from 'web3';
 
 // const web3 = new Web3(new Web3.providers.HttpProvider('https://data-seed-prebsc-1-s1.binance.org:8545/'));
 const web3 = new Web3(window.ethereum);
-window.ethereum.enable();
+// window.ethereum.enable();
 
 export default web3;
