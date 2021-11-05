1e1b3e6d3ae204d59c2ad3b24267570a1b32b5f8
bobpepers <bob.pepers@gmail.com>
2021-11-05 08:10:21 +0100

    up

--- src/app/config/abis/index.js
+++ src/app/config/abis/index.js
@@ -5,10 +5,15 @@ import discovery from './discovery.json';
 import ierc20 from './ierc20.json';
 import pool from './pool.json'
 
+import quickFarm from './quickFarm.json';
+import quickDiscovery from './quickDiscovery.json'
+
 export default {
   discovery,
   farm,
   pool,
   wrunes,
   ierc20,
+  quickFarm,
+  quickDiscovery,
 }
--- src/app/config/pool/pool.matic-mainnet.js
+++ src/app/config/pool/pool.matic-mainnet.js
@@ -2,8 +2,8 @@ import features from './features'
 import abis from '../abis'
 import { addresses } from '../constants/addresses.bsc-testnet'
 import { convertFromUnits } from '../../utils/bignumbers';
-import Cake from '../../assets/images/cake.png';
-import Bake from '../../assets/images/bake.png';
+import Quick from '../../assets/images/quick.png';
+// import Bake from '../../assets/images/bake.png';
 
 const getTokenSwapLink = (output, input) => {
   if (input) {
@@ -13,8 +13,46 @@ const getTokenSwapLink = (output, input) => {
   return `https://test-exchange-pancake.pages.dev/#/swap?outputCurrency=${output}`
 }
 
-const getId = (str) => `80001_${str}`
+const getId = (str) => `137_${str}`
 
 export default [
-
+  {
+    id: getId('0'),
+    name: 'Stake QUICK Earn wRUNES',
+    type: 'Farm',
+    fees: {
+      entry: 0,
+      other: 0,
+      exit: 0,
+    },
+    nepRewardAllocation: convertFromUnits(500000000000000000000000),
+    token: {
+      decimals: 18,
+      address: addresses.tokens.QUICK,
+      abi: abis.ierc20,
+    },
+    discovery: {
+      address: addresses.contracts.v1.QUICKDISCOVERY,
+      abi: abis.quickDiscovery,
+    },
+    farm: {
+      address: addresses.contracts.v1.QUICKFARM,
+      abi: abis.quickFarm,
+    },
+    features: [features.NO_FEE, features.PANCAKESWAP],
+    hot: true,
+    defaultPriority: 0,
+    live: true,
+    isLPToken: false,
+    lockingPeriod: '24 hours',
+    tokenSwapLink: getTokenSwapLink(addresses.tokens.QUICK),
+    assets: {
+      logo: Quick,
+      background: '/patterns/pyramid.svg',
+    },
+    symbol: {
+      reward: 'wRUNES',
+      liquidity: 'QUICK',
+    },
+  },
 ]
